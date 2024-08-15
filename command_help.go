3db0dd39644006624d7c2d511ebc27ff5c919fb5
gwarzecha <gmwarzecha@tutanota.com>
2024-08-14 17:18:42 -0700

    Set up REPL, exit and help commands

---
+++ command_help.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to your Pokedex!")
+	fmt.Println()
+	fmt.Println("Usage:")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
